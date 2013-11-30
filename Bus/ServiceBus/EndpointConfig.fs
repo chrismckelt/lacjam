5c424460f767f6ceeaffab2e1a0833a51b3a1e6f
chrismckelt <chris@mckelt.com>
2013-11-30 18:49:30 +1100

    NSB no warnings

--- Bus/ServiceBus/EndpointConfig.fs
+++ Bus/ServiceBus/EndpointConfig.fs
@@ -1,26 +1,22 @@
 namespace Lacjam
 module ServiceBus =
-open System
-open NServiceBus
+    open System
+    open NServiceBus
 
-    let Bus =  (Configure.With()
-                    .DefaultBuilder()
-                    .DefineEndpointName("lacjam.worker")
-                    .Log4Net()
-                    .XmlSerializer()
-                    .IsTransactional(true)
-                    .PurgeOnStartup(false)
-                    .Sagas()
-                    .DisableTimeoutManager()
-                    .UnicastBus()
-                    .CreateBus()
-                    .Start() ) 
+        let Bus =  (NServiceBus.Configure.With()
+                        .DefaultBuilder()
+                        .UseTransport<Msmq>()
+                        .DefineEndpointName("lacjam.worker")
+                        .Log4Net()
+                        .PurgeOnStartup(false)
+                        .DisableTimeoutManager()
+                        .UnicastBus())
 
-    type EndpointConfig() =
-        interface AsA_Server  
-        interface IConfigureThisEndpoint 
-        interface IWantCustomInitialization with
-            member this.Init() = (Bus) |> ignore
+        type EndpointConfig() =
+            interface AsA_Server  
+            interface IConfigureThisEndpoint 
+            interface IWantCustomInitialization with
+                member this.Init() = (Bus) |> ignore
                    
                   
 
