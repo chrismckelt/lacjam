7bc0e25297c5af5ff6fc6aa10d784a7241a2f203
Chris McKelt <chris@mckelt.com>
2013-12-09 12:11:44 +1100

    Scheduling working with Autofac

--- App/ServiceBus/EndpointConfig.fs
+++ App/ServiceBus/EndpointConfig.fs
@@ -1,27 +1,15 @@
 namespace Lacjam.ServiceBus 
     open System
+    open Autofac
     open NServiceBus
+    open NServiceBus.Features
     open Lacjam.Core
     open Lacjam.Core.Jobs
+
     module Startup = 
 
-        NServiceBus.Configure.ScaleOut(new Action<Settings.ScaleOutSettings>(fun a -> (a.UseSingleBrokerQueue()|>ignore)))
-        NServiceBus.Configure.Transactions.Enable() |> ignore
-
-        let Bus = NServiceBus.Configure.With()
-                        .Log4Net()
-                        .DefaultBuilder()                        
-                        .InMemorySagaPersister()
-                        .InMemoryFaultManagement()      
-                        .UseTransport<Msmq>()
-                        .DefineEndpointName("lacjam")
-                        .UnicastBus()
-                        .LoadMessageHandlers()
-                        .DoNotCreateQueues()
-                        .PurgeOnStartup(false)
-                        .CreateBus()
-                        .Start(fun _ -> Configure.Instance.ForInstallationOn<NServiceBus.Installation.Environments.Windows>().Install() |> ignore)
-
+        let ContainerBuilder = new ContainerBuilder()
+        let Container = ContainerBuilder.Build()
 
         [<Serializable>]
         type TestPoll() = 
@@ -32,17 +20,38 @@ namespace Lacjam.ServiceBus
         type EndpointConfig() =
             interface IConfigureThisEndpoint
             interface AsA_Server
+            interface IWantCustomInitialization with
+                member this.Init() = 
+                    Configure.ScaleOut(new Action<Settings.ScaleOutSettings>(fun a -> (a.UseSingleBrokerQueue()|>ignore)))
+                    Configure.Transactions.Enable() |> ignore
+                    Configure.Transactions.Enable() |> ignore
+                    Configure.Serialization.Xml() |> ignore
+                    Configure.Features.Enable<Sagas>() |> ignore
 
+                    Configure.With()
+                        .DefineEndpointName("lacjam.servicebus")
+                        .Log4Net()
+                        .AutofacBuilder(Container)                   
+                        .InMemorySagaPersister()
+                        .InMemoryFaultManagement()      
+                        .UseTransport<Msmq>()
+                        .DoNotCreateQueues()
+                        .PurgeOnStartup(false)
+                        .UnicastBus() |> ignore
 
          type ServiceBusStartUp() =              
             interface IWantToRunWhenBusStartsAndStops with
                 member this.Start() = 
                     Console.WriteLine("-- Service Bus Started --")
-                    //let message = new Lacjam.Core.Messages.BedlamPoll()
-                    let message = new TestPoll()
-                    do Bus.Send(message :> IMessage) |> ignore
+                    let message = new Lacjam.Core.Messages.BedlamPoll()
+                    //let message = new TestPoll()
+                    let bus = Container.Resolve<IBus>()
+                    do bus.Send(message :> IMessage) |> ignore
                     let d = Convert.ToDouble(30)
-                    Schedule.Every(System.TimeSpan.FromSeconds(d)).Action(fun a->Console.WriteLine("Another 30 seconds have elapsed."))
+                    Schedule.Every(System.TimeSpan.FromSeconds(d)).Action(fun a->
+                                                                Console.WriteLine("Another 30 seconds have elapsed.")
+                                                                do bus.Send(message :> IMessage) |> ignore
+                                                                )
                 member this.Stop() = (Console.WriteLine("-- Service Bus Stopped --"))
    
 
