b1e46a020c6dd2f115317408089f1e263b5d059f
chrismckelt <chris@mckelt.com>
2013-12-22 20:05:50 +1100

    ilogwrite ioc unit test

---
+++ Tests/Core.UnitTests/RuntimeTestFixture.fs
@@ -0,0 +1,28 @@
+﻿module RuntimeTestFixture
+
+open Autofac
+open FsUnit.Xunit
+open Lacjam
+open Lacjam.Core
+open Lacjam.Core.Runtime
+open Microsoft.FSharp.Control
+open System
+module RuntimeTestFixture  =
+
+    open System.Diagnostics
+    open Xunit
+
+    [<Literal>]
+    let url = "http://www.bedlam.net.au"
+
+    type Message = int * AsyncReplyChannel<int>
+
+    let formatString = "Message number {0} was received. Message contents: {1}"
+    let printThreadId note =
+        // Append the thread ID.
+        printfn "%d : %s" System.Threading.Thread.CurrentThread.ManagedThreadId note
+
+    [<Fact>]
+    let ``IOC contains ILogWriter``() =
+        let found = Lacjam.Core.Runtime.Ioc.Resolve<ILogWriter>()
+        found |> should not' (Null)
---
+++ Tests/ServiceBus.UnitTests/EndpointConfigTestFixture.fs
@@ -0,0 +1,13 @@
+﻿module EndpointConfigTestFixture
+open System
+open System.Diagnostics
+open Microsoft.FSharp.Control
+open Xunit
+open FsUnit.Xunit
+open Lacjam
+open Lacjam.Core
+open Lacjam.ServiceBus
+
+[<Fact>] 
+let ``FSharp Test`` () =
+    Domain.z().ToString().Length |> should equal 3
