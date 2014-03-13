66fab459b3a4b4fbcf339e525681d6185cf11f10
Chris McKelt <chris@mckelt.com>
2014-03-13 18:42:35 +1100

    up

--- scripts/Lacjam-Deploy.bat
+++ scripts/Lacjam-Deploy.bat
@@ -1,4 +1,4 @@
 net stop lacjam.servicebus
-copy "C:\dev\Lacjam\src\ServiceBus\bin\Debug" "C:\software\Lacjam-Roadmap"
+copy "C:\dev\Lacjam\src\ServiceBus\bin\Debug" "C:\software\Lacjam-Deploy"
 net start lacjam.servicebus
 pause
