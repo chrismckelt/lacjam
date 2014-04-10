e6cfcdb8100f5a5056814b60308ce6b3658ca715
Chris McKelt <chris@mckelt.com>
2014-04-10 22:00:06 +0800

    bootstrap ui

--- src/WebClient/App/app.js
+++ src/WebClient/App/app.js
@@ -9,36 +9,57 @@
 /// <reference path="../scripts/typings/bootstrap/bootstrap.d.ts" />
 /// <reference path="../scripts/typings/bootstrap.datepicker/bootstrap.datepicker.d.ts" />
 /// <reference path="../scripts/typings/jquery/jquery.d.ts" />
-'use strict';
-define(['services/routeResolver'], function () {
-    var app = angular.module('lacjam', ['ngRoute', 'ngAnimate', 'routeResolverServices', 'wc.Directives', 'wc.Animations', 'ui.bootstrap']);
+var app;
+(function (app) {
+    'use strict';
 
-    app.config([
-        '$routeProvider', 'routeResolverProvider', '$controllerProvider', '$compileProvider', '$filterProvider', '$provide', '$httpProvider',
-        function ($routeProvider, routeResolverProvider, $controllerProvider, $compileProvider, $filterProvider, $provide, $httpProvider) {
-            //Change default views and controllers directory using the following:
-            //routeResolverProvider.routeConfig.setBaseDirectories('/app/views', '/app/controllers');
-            app.register = {
-                controller: $controllerProvider.register,
-                directive: $compileProvider.directive,
-                filter: $filterProvider.register,
-                factory: $provide.factory,
-                service: $provide.service
-            };
+    var lac = angular.module('app', ['ngRoute', 'ui.bootstrap']);
 
-            //Define routes - controllers will be loaded dynamically
-            var route = routeResolverProvider.route;
+    lac.controller('controller', (function () {
+    }));
 
-            $routeProvider.when('/customers', route.resolve('Customers', 'customers/')).when('/customerorders/:customerID', route.resolve('CustomerOrders', 'customers/')).when('/customeredit/:customerID', route.resolve('CustomerEdit', 'customers/')).when('/orders', route.resolve('Orders', 'orders/')).when('/about', route.resolve('About')).otherwise({ redirectTo: '/customers' });
-        }]);
+    lac.service('service', (function () {
+    }));
 
-    //Only needed for Breeze. Maps Q (used by default in Breeze) to Angular's $q to avoid having to call scope.$apply()
-    app.run([
-        '$q', '$rootScope',
-        function ($q, $rootScope) {
-            breeze.core.extendQ($rootScope, $q);
-        }]);
+    lac.directive('directive', (function () {
+    }));
 
-    return app;
-});
+    lac.config([
+        '$routeProvider', function ($routeProvider) {
+            $routeProvider.when('/', { templateUrl: 'index.cshtml' }).otherwise({ redirectTo: '/' });
+        }
+    ]);
+})(app || (app = {}));
+//'use strict';
+//define(['services/routeResolver'], function () {
+//    var app = angular.module('lacjam', ['ngRoute', 'ngAnimate', 'routeResolverServices', 'wc.Directives', 'wc.Animations', 'ui.bootstrap']);
+//    app.config(['$routeProvider', 'routeResolverProvider', '$controllerProvider', '$compileProvider', '$filterProvider', '$provide', '$httpProvider',
+//        function ($routeProvider, routeResolverProvider, $controllerProvider, $compileProvider, $filterProvider, $provide, $httpProvider) {
+//            //Change default views and controllers directory using the following:
+//            //routeResolverProvider.routeConfig.setBaseDirectories('/app/views', '/app/controllers');
+//            //app.register =
+//            //{
+//            //    controller: $controllerProvider.register,
+//            //    directive: $compileProvider.directive,
+//            //    filter: $filterProvider.register,
+//            //    factory: $provide.factory,
+//            //    service: $provide.service
+//            //};
+//            //Define routes - controllers will be loaded dynamically
+//            var route = routeResolverProvider.route;
+//            $routeProvider
+//            //route.resolve() now accepts the convention to use (name of controller & view) as well as the
+//            //path where the controller or view lives in the controllers or views folder if it's in a sub folder.
+//            //For example, the controllers for customers live in controllers/customers and the views are in views/customers.
+//            //The controllers for orders live in controllers/orders and the views are in views/orders
+//            //The second parameter allows for putting related controllers/views into subfolders to better organize large projects
+//            //Thanks to Ton Yeung for the idea and contribution
+//                .when('/customers', route.resolve('Customers', 'customers/'))
+//                .when('/customerorders/:customerID', route.resolve('CustomerOrders', 'customers/'))
+//                .when('/customeredit/:customerID', route.resolve('CustomerEdit', 'customers/'))
+//                .when('/orders', route.resolve('Orders', 'orders/'))
+//                .when('/about', route.resolve('About'))
+//                .otherwise({ redirectTo: '/customers' });
+//        }]);
+//});
 //# sourceMappingURL=app.js.map
--- src/WebClient/App/app.ts
+++ src/WebClient/App/app.ts
@@ -12,52 +12,62 @@
 /// <reference path="../scripts/typings/jquery/jquery.d.ts" />
 
 
-'use strict';
+module app {
+    'use strict';
 
-define(['services/routeResolver'], function () {
+    var lac: ng.IModule = angular.module('app', ['ngRoute', 'ui.bootstrap']);
 
-    var app = angular.module('lacjam', ['ngRoute', 'ngAnimate', 'routeResolverServices', 'wc.Directives', 'wc.Animations', 'ui.bootstrap']);
+    lac.controller('controller', (function() {}));
 
-    app.config(['$routeProvider', 'routeResolverProvider', '$controllerProvider', '$compileProvider', '$filterProvider', '$provide', '$httpProvider',
-        function ($routeProvider, routeResolverProvider, $controllerProvider, $compileProvider, $filterProvider, $provide, $httpProvider) {
+    lac.service('service', (function () { }));
 
-            //Change default views and controllers directory using the following:
-            //routeResolverProvider.routeConfig.setBaseDirectories('/app/views', '/app/controllers');
+    lac.directive('directive', (function () { }));
 
-            app.register =
-            {
-                controller: $controllerProvider.register,
-                directive: $compileProvider.directive,
-                filter: $filterProvider.register,
-                factory: $provide.factory,
-                service: $provide.service
-            };
+    lac.config([
+        '$routeProvider', function($routeProvider: ng.route.IRouteProvider) {
+            $routeProvider.when('/', { templateUrl: 'index.cshtml' }).
+                otherwise({ redirectTo: '/' });
+        }
+    ]);
+}
 
-            //Define routes - controllers will be loaded dynamically
-            var route = routeResolverProvider.route;
+//'use strict';
 
-            $routeProvider
-            //route.resolve() now accepts the convention to use (name of controller & view) as well as the 
-            //path where the controller or view lives in the controllers or views folder if it's in a sub folder. 
-            //For example, the controllers for customers live in controllers/customers and the views are in views/customers.
-            //The controllers for orders live in controllers/orders and the views are in views/orders
-            //The second parameter allows for putting related controllers/views into subfolders to better organize large projects
-            //Thanks to Ton Yeung for the idea and contribution
-                .when('/customers', route.resolve('Customers', 'customers/'))
-                .when('/customerorders/:customerID', route.resolve('CustomerOrders', 'customers/'))
-                .when('/customeredit/:customerID', route.resolve('CustomerEdit', 'customers/'))
-                .when('/orders', route.resolve('Orders', 'orders/'))
-                .when('/about', route.resolve('About'))
-                .otherwise({ redirectTo: '/customers' });
+//define(['services/routeResolver'], function () {
 
-        }]);
+//    var app = angular.module('lacjam', ['ngRoute', 'ngAnimate', 'routeResolverServices', 'wc.Directives', 'wc.Animations', 'ui.bootstrap']);
 
-    //Only needed for Breeze. Maps Q (used by default in Breeze) to Angular's $q to avoid having to call scope.$apply() 
-    app.run(['$q', '$rootScope',
-        function ($q, $rootScope) {
-            breeze.core.extendQ($rootScope, $q);
-        }]);
+//    app.config(['$routeProvider', 'routeResolverProvider', '$controllerProvider', '$compileProvider', '$filterProvider', '$provide', '$httpProvider',
+//        function ($routeProvider, routeResolverProvider, $controllerProvider, $compileProvider, $filterProvider, $provide, $httpProvider) {
 
-    return app;
+//            //Change default views and controllers directory using the following:
+//            //routeResolverProvider.routeConfig.setBaseDirectories('/app/views', '/app/controllers');
 
-});
+//            //app.register =
+//            //{
+//            //    controller: $controllerProvider.register,
+//            //    directive: $compileProvider.directive,
+//            //    filter: $filterProvider.register,
+//            //    factory: $provide.factory,
+//            //    service: $provide.service
+//            //};
+
+//            //Define routes - controllers will be loaded dynamically
+//            var route = routeResolverProvider.route;
+
+//            $routeProvider
+//            //route.resolve() now accepts the convention to use (name of controller & view) as well as the 
+//            //path where the controller or view lives in the controllers or views folder if it's in a sub folder. 
+//            //For example, the controllers for customers live in controllers/customers and the views are in views/customers.
+//            //The controllers for orders live in controllers/orders and the views are in views/orders
+//            //The second parameter allows for putting related controllers/views into subfolders to better organize large projects
+//            //Thanks to Ton Yeung for the idea and contribution
+//                .when('/customers', route.resolve('Customers', 'customers/'))
+//                .when('/customerorders/:customerID', route.resolve('CustomerOrders', 'customers/'))
+//                .when('/customeredit/:customerID', route.resolve('CustomerEdit', 'customers/'))
+//                .when('/orders', route.resolve('Orders', 'orders/'))
+//                .when('/about', route.resolve('About'))
+//                .otherwise({ redirectTo: '/customers' });
+
+//        }]);
+//});
