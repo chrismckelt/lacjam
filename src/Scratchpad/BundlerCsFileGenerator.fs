ffd72410ed46c5ba07c2f3e6d039cf7ecf387cb1
Chris McKelt <chris@mckelt.com>
2014-12-29 15:53:06 +0800

    up

---
+++ src/Scratchpad/BundlerCsFileGenerator.fs
@@ -0,0 +1,29 @@
+﻿module BundlerCsFileGenerator
+                                    #if INTERACTIVE
+namespace BundlerCs
+#endif
+
+module DatabaseBackup 
+
+
+#if INTERACTIVE
+=
+#r "System.dll"
+#r "System.Data.dll"
+#l "C:\Program Files (x86)\Reference Assemblies\Microsoft\FSharp\.NETFramework\v4.0\4.3.1.0\FSharp.Core.dll"
+#r "FSharp.Data.dll"
+#r "FSharp.Data.TypeProviders.dll"
+#r "System.Data.Linq.dll"
+#endif
+
+    open System
+    open System.Data
+    open System.IO
+    open System.Data
+    open System.Data.SqlClient
+    open FSharp
+    open FSharp.Data
+    open FSharp.Data.SqlClient
+    open FSharp.IO
+    open Microsoft.FSharp.Data.TypeProviders
+    open Xunit
